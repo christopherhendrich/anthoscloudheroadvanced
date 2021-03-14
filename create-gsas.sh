1bb8d81f5af9594f20351457956291f1d5371a0c
christopherhendrich <50594177+christopherhendrich@users.noreply.github.com>
2021-03-14 12:31:15 -0700

    Add files via upload

--- create-gsas.sh
+++ create-gsas.sh
@@ -5,7 +5,6 @@ export CTX_1=gke_${PROJECT_ID}_us-central1-a_cymbal-bank-prod
 export CTX_2=gke_${PROJECT_ID}_us-central1-a_cymbal-bank-dev
 export CTX_3=gke_${PROJECT_ID}_us-central1-a_m4a-processing
 export CTX_4=gke_${PROJECT_ID}_us-central1-a_cymbal-monolith-cluster
-gcloud container clusters get-credentials cymbal-monolith-cluster --zone us-central1-a --project ${PROJECT_ID} 
 
 gcloud iam service-accounts create m4a-install \
  --project=${PROJECT_ID}
